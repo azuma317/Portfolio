d799a4f2af5c4ac54b9543b369ff12f29683451d
azuma sato <lifeghtcloerce5291@gmail.com>
2023-06-04 17:49:07 +0900

    Add Contentlayer

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { Post, allPosts } from "contentlayer/generated";
+import { format, parseISO } from "date-fns";
+
+export async function getStaticPaths() {
+  const paths = allPosts.map((post) => post.url)
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post) => post._id === params.slug)
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+const PostLayout = ({ post }: { post: Post }) => {
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+      <article className="mx-auto max-w-2xl py-16">
+        <div className="mb-6 text-center">
+          <Link href="/">
+            <a className="text-center text-sm font-bold uppercase text-blue-700">Home</a>
+          </Link>
+        </div>
+        <div className="mb-6 text-center">
+          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
+          <time dateTime={post.date} className="text-sm text-slate-600">
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
+          </time>
+        </div>
+        <div className="cl-post-body" dangerouslySetInnerHTML={{ __html: post.body.html }}></div>
+      </article>
+    </>
+  )
+}
+
+export default PostLayout
---
+++ contentlayer.config.ts
@@ -0,0 +1,33 @@
+import { makeSource, defineDatabase } from 'contentlayer-source-notion'
+import { Client } from '@notionhq/client'
+
+const client = new Client({ auth: process.env.NOTION_TOKEN })
+
+export const Post = defineDatabase(() => ({
+  name: 'Post',
+  databaseId: '9b43a29a72bf4b9eb176ae80be451400',
+  query: {
+    filter: {
+      property: 'Status',
+      status: {
+        equals: 'Published',
+      },
+    },
+  },
+  properties: {
+    date: {
+      name: 'Created time',
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (post) => `/posts/${post._id}`,
+    },
+  },
+}))
+
+export default makeSource({
+  client,
+  databaseTypes: [Post],
+})
