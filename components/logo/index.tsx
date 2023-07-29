1e9edfc591c3d6bd10b3afee60dccc28acba29be
azuma sato <lifeghtcloerce5291@gmail.com>
2023-07-29 22:48:42 +0900

    Fix logo image

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link, { LinkProps as NextLinkProps } from 'next/link';
-import Image from "next/image";
 import { name, avatar } from '@/lib';
 
 interface LogoProps extends NextLinkProps {
@@ -11,13 +10,12 @@ interface LogoProps extends NextLinkProps {
 export default function Logo(props: LogoProps) {
   return (
     <Link {...props} aria-label="Azuma" className={props.className}>
-      <Image
+      <img
+        src="/static/avatar.jpg"
         alt={name}
         className="rounded-full grayscale border-solid border-4 border-neutral-200 dark:border-white"
-        src={avatar}
         placeholder="blur"
         width={48}
-        priority
       />
     </Link>
   )
