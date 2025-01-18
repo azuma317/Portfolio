d6fd8f8138f1a8e6d031d15b544583e03c90e8f1
azuma sato <lifeghtcloerce5291@gmail.com>
2025-01-18 22:26:02 +0900

    Layoutの修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,12 +55,12 @@ export default function RootLayout({
     <html
       lang="ja"
       className={cx(
-        "text-black bg-white dark:text-white dark:bg-black",
+        "text-black dark:text-white",
         playfair.variable,
         lora.variable,
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased h-screen max-w-xl mx-4 lg:mx-auto">
         <Theme
           accentColor="mint"
           grayColor="gray"
@@ -69,7 +69,7 @@ export default function RootLayout({
           radius="full"
           appearance="dark"
         >
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <main className="flex-auto min-w-0 flex flex-col px-2 md:px-0 h-full">
             <Navbar />
             {children}
             <Footer />
