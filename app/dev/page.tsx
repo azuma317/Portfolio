c4bf10582f51c72f398822009b40b7f9d0793348
azuma sato <lifeghtcloerce5291@gmail.com>
2025-01-19 16:39:46 +0900

    不要なプログラムの削除

--- app/dev/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Dev() {
-  return (
-    <section>
-      <h1 className="mb-8 font-semibold text-2xl tracking-tighter">Dev</h1>
-    </section>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
-import { about, bio, name } from './lib/info';
+import Link from 'next/link';
 
 export default function Home() {
+  const yearsSinceStart =
+    new Date().getFullYear() - new Date('2019').getFullYear();
+
   return (
     <section>
-      <h1 className="mb-8 font-bold text-2xl tracking-tighter">{name}</h1>
+      <h1 className="mb-8 font-bold text-2xl tracking-tighter">Azuma</h1>
       <p className="my-4 max-w-[500px] text-neutral-800 dark:text-neutral-200">
-        {about()}
+        I'm currently based in Fukuoka and deeply involved in a diverse range of
+        exciting{' '}
+        <Link href="/projects">
+          <b>projects</b>
+        </Link>{' '}
+        and{' '}
+        <Link href="/blog">
+          <b>interests</b>
+        </Link>
+        .
       </p>
 
       <p className="my-4 max-w-[600px] text-neutral-800 dark:text-neutral-200">
-        {bio()}
+        As a software developer, I've been writing code for over{' '}
+        <b>{yearsSinceStart - 1} years</b>. While my expertise spans across the
+        full software development stack, I am particularly passionate about
+        Infrastructure Development.{' '}
+        <Link href="/dev">
+          <b>Read on</b>
+        </Link>
+        .
       </p>
     </section>
   );
