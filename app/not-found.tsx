8ae1bf1aa177f4fff825835d2c73249b76939ab7
azuma sato <lifeghtcloerce5291@gmail.com>
2025-01-19 11:21:12 +0900

    404ページの修正

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,20 @@
 export default function notFound() {
   return (
-    <section>
-      <h1 className="mb-8 font-semibold text-2xl tracking-tighter">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+    <section className="flex flex-col items-center justify-center min-h-screen px-4 py-16 text-center">
+      <h1 className="mb-4 text-6xl font-normal text-white">404</h1>
+      <p className="mb-8 text-4xl font-serif text-white">
+        Out of nothing, something.
+      </p>
+      <p className="max-w-2xl mb-8 text-xl text-gray-300">
+        It seems you've reached a page that doesn't exist. Perhaps you'd like to
+        explore my projects or learn more about me?
+      </p>
+      <a
+        href="/"
+        className="text-xl text-white underline hover:text-gray-300 transition-colors"
+      >
+        Home
+      </a>
     </section>
   );
 }
