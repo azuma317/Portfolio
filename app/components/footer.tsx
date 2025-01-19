d70c2aafc4df4937b72dffca57f0677450216a6a
azuma sato <lifeghtcloerce5291@gmail.com>
2025-01-19 11:13:13 +0900

    Xのリンクを追加

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,7 +30,18 @@ export default function Footer() {
             href="https://github.com/azuma317"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7">GitHub</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/azumax_develop"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">Follow me on X</p>
           </a>
         </li>
       </ul>
