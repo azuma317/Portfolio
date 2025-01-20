c2d95837146a213e401ad79d66d023aa3ac73cfd
azuma sato <lifeghtcloerce5291@gmail.com>
2025-01-20 22:21:06 +0900

    記事の追加

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -22,7 +22,7 @@ export function BlogPosts() {
             href={`/blog/${post.slug}`}
           >
             <div className="flex w-full flex-col space-x-0 md:flex-row md:space-x-2">
-              <p className="w-[120px] text-neutral-600 tabular-nums dark:text-neutral-400">
+              <p className="w-[160px] text-neutral-600 tabular-nums dark:text-neutral-400">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 tracking-tight dark:text-neutral-100 group-hover:text-neutral-800 dark:group-hover:text-emerald-400">
